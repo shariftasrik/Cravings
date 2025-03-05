ff5397c45a25759aae74d1d52d06fe7f19684a15
Nimur Rahman Sharif Tasrik <126714137+shariftasrik@users.noreply.github.com>
2025-03-05 16:41:40 +0600

    Product model, database, sidebar option and route added

--- routes/web.php
+++ routes/web.php
@@ -103,3 +103,14 @@ Route::middleware('client')->group(function () {
     
 });
 // End Client Middleware
+
+
+Route::controller(RestaurantController::class)->group(function(){
+    Route::get('/all/product', 'AllProduct')->name('all.product');
+    Route::get('/add/menu', 'AddMenu')->name('add.menu');
+    Route::post('/store/menu', 'StoreMenu')->name('menu.store');
+    Route::get('/edit/menu/{id}', 'EditMenu')->name('edit.menu');
+    Route::post('/update/menu', 'UpdateMenu')->name('menu.update');
+    Route::get('/delete/menu/{id}', 'DeleteMenu')->name('delete.menu');
+});
+// End Restaurent Middleware
